50790053714a9fbb8de3d7e8f35c18b2a055a814
Nicolasmaes <n.maes@codeur.online>
2021-07-06 13:32:23 +0200

    ajout du scroll effect sur la bibliothèque JS

--- back/db_connect.php
+++ back/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = 'localhost';
 $dbname = 'back_office';
-$username = 'nicolasm';
-$password = 'g0Wr1plazhzf1Q==';
+$username = 'root';
+$password = '';
 try {
     $db = new PDO("mysql:host=$servername; dbname=$dbname",$username,$password);
     $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- front/pages/jslibrary.php
+++ front/pages/jslibrary.php
@@ -1,21 +1,21 @@
 <?php include "../../includes/header.php";
 session_start();?>
 <div class="embed_collection">
-<embed src="../../assets/javascript/1-color-square-changing/" type="">
-<embed src="../../assets/javascript/defi-03-05_fizzBuzz/" type="">
-<embed src="../../assets/javascript/defi-04-05_hambuger_menu/" type="">
-<embed src="../../assets/javascript/defi-07-05_palindromeCheck/" type="">
-<embed src="../../assets/javascript/defi-12-04_headsOrTails/" type="">
-<embed src="../../assets/javascript/defi-12-05_scrollIndicator/" type="">
-<embed src="../../assets/javascript/defi-13-04_passwordHideAndShow/" type="">
-<embed src="../../assets/javascript/defi-14-04_checkCapsAndNumlock/" type="">
-<embed src="../../assets/javascript/defi-15-04_checkPassword/" type="">
-<embed src="../../assets/javascript/defi-16-04_copyPasteText/" type="">
-<embed src="../../assets/javascript/defi-17-05-21_navbarDisplay/" type="">
-<embed src="../../assets/javascript/defi-18-05_sortByFilter/" type="">
-<embed src="../../assets/javascript/defi-22-04_arrowFunction/" type="">
-<embed src="../../assets/javascript/defi-26-04_clock/" type="">
-<embed src="../../assets/javascript/defi-28-04_carousel/" type="">
-<embed src="../../assets/javascript/expose-JS-switch/" type="">
+<embed src="../../assets/javascript/1-color-square-changing/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-03-05_fizzBuzz/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-04-05_hambuger_menu/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-07-05_palindromeCheck/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-12-04_headsOrTails/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-12-05_scrollIndicator/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-13-04_passwordHideAndShow/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-14-04_checkCapsAndNumlock/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-15-04_checkPassword/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-16-04_copyPasteText/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-17-05-21_navbarDisplay/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-18-05_sortByFilter/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-22-04_arrowFunction/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-26-04_clock/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/defi-28-04_carousel/" type="" data-aos="fade-up">
+<embed src="../../assets/javascript/expose-JS-switch/" type="" data-aos="fade-up">
 </div>
 <?php include "../../includes/footer.php"; ?>
