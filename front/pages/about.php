b36f6fc166cd2b2a1a17f57c076c3e51eef717f7
Nicolasmaes <n.maes@codeur.online>
2021-11-03 16:43:56 +0100

    correction de la bibliothèque JS, de la page "à propos"

--- front/pages/about.php
+++ front/pages/about.php
@@ -1,14 +1,17 @@
 <?php include "../../includes/header.php";
-session_start();?>
-<div class="container_about">
-        <div class="about_me">
-        <p>J'ai 32 ans et j'habite à Dijon.<br>
-        Je suis en formation actuellement à l'Access Code School de Dijon. Avant ça, j'étais chef réceptionniste dans un hôtel pendant 7 ans. J'ai beaucoup d'expérience dans la relation client. Aujourd'hui je me reconvertis en me lancant dans un domaine qui m'a toujours intéressé.</p><br>
-        <p>Mon but est de devenir développeur web et web mobile. Je suis prêt à prendre tous les conseils que l'on voudra bien me donner et je suis prêt à travailler énormément pour réussir.</p>
-        </div><!-- ferme about_me -->
-        <br><br>
-        <div class="about_sports">
-        <p>Très sportif, j'essaye chaque année de parcourir plus de kilomètres à pied, en vélo et en natation ! J'ai couru 3 marathons et 1 triathlon jusqu'ici. Vivement la reprise des courses pour se fixer de nouveaux objectifs !</p>
-        </div><!-- ferme about_sports -->
-</div><!-- ferme container_about -->
+session_start(); ?>
+<div class="article">
+        <div class="article__group">
+                <div class="about_me">
+                        <p>J'ai 32 ans et j'habite à Dijon.<br>
+                                Je suis en formation actuellement à l'Access Code School de Dijon. Avant ça, j'étais chef réceptionniste dans un hôtel pendant 7 ans. J'ai beaucoup d'expérience dans la relation client. Aujourd'hui je me reconvertis en me lancant dans un domaine qui m'a toujours intéressé.</p><br>
+                        <p>Mon but est de devenir développeur web et web mobile. Je suis prêt à prendre tous les conseils que l'on voudra bien me donner et je suis prêt à travailler énormément pour réussir.</p>
+                </div><!-- ferme about_me -->
+                <br>
+                <div class="about_sports">
+                        <p>Très sportif, j'essaye chaque année de parcourir plus de kilomètres à pied, en vélo et en natation ! J'ai couru 3 marathons et 1 triathlon jusqu'ici. Vivement la reprise des courses pour se fixer de nouveaux objectifs !</p>
+                </div><!-- ferme about_sports -->
+        </div><!-- ferme container_about -->
+</div>
+</div>
 <?php include "../../includes/footer.php"; ?>
